9e30b4415f11b913974fa6a9000d9884441dbe39
Ali Koptan <ali.koptan0@gmail.com>
2019-05-10 20:40:03 +0200

    seperate parser modifications

--- seperate-parser/src/seperate-parser.cpp
+++ seperate-parser/src/seperate-parser.cpp
@@ -16,16 +16,21 @@ class node {
         void add(char ch) {
             header += ch;
         }
+        void destroy() {
+        	id = "";
+        	header = "";
+        	children.clear();
+        }
 };
 
-bool isMultiKeyword(string);
+bool isMultiKeyword(int);
 void clearExtraSpaces(string&);
 void initialize();
 void finalize();
 void normalize();
 void read();
+pair <vector <node>, int> parse(int);
 void exportNormal();
-vector <node> parse(int index, node&);
 void traverse(node);
 
 string megaToken = "";
@@ -42,7 +47,10 @@ int main() {
 	normalize();
 	clearExtraSpaces(megaToken);
 	exportNormal();
-	parse(0, program);
+
+	int index = 0;
+	program.header = "PROGRAM";
+	program.children = parse(index).first;
 	traverse(program);
 	finalize();
 }
@@ -89,7 +97,7 @@ void normalize() {
 		string smallToken = "";
 		bool hold = false;
 		for (unsigned int j = 0; j < input[i].size(); j++) {
-			if (!hold) {
+			if (hold == false) {
 				if (input[i][j] == ' ') {
 					smallToken += " ";
 					hold = true;
@@ -147,55 +155,85 @@ string toString(int num) {
     return numString;
 }
 
-int isMultiKeyword(string str) {
-	for (unsigned int i = 0; i < 5; i++) {
-		if (str == multiKeywordList[i])
-			return true;
+bool isMultiKeyword(int index) {
+	for (unsigned int i = 0; i < 6; i++) {
+		if (index + multiKeywordList[i].size() < megaToken.size()) {
+			string str = megaToken.substr(index, multiKeywordList[i].size());
+			if (str == multiKeywordList[i])
+				return true;
+		}
 	}
 	return false;
 }
-vector <node> parse(int index, node &parent) {
-	vector <node> children;
-	node smallToken;
-	for (unsigned int i = index; i < megaToken.size(); i++) {
-		smallToken.add(megaToken[i]);
-		if (megaToken[i] == ';') {
-			children.push_back(smallToken);
-			smallToken.header = "";
+
+pair <string, unsigned int> crawl(int index) {
+	pair <string, int> token;
+	for (int i = index; i < megaToken.size(); i++) {
+		token.first += megaToken[i];
+		if (megaToken[i] == ';' || (megaToken[i] == '>' && megaToken[i - 1] == 'h')) {
+			token.second = i + 2; //jump 4 lines.
+			return token;
 		}
-		if (megaToken[i] == '{') {
-			parse(index + 1, smallToken);
-		}
-		if (megaToken[i] == '}') {
-			return children;
+		if (megaToken[i + 1] == '{') {
+			token.second = i + 3; //these numbers are based on Normalize() function, number
+			return token;		  // of spaces between words is constant.
 		}
 	}
+	return token;
+}
+
+pair <vector <node>, int> parse(int index) {
+	vector <node> children;
+	node child;
+	string web = crawl(index).first;
+	int resIndex = crawl(index).second;
+	if (isMultiKeyword(resIndex)) {
+
+	}
+
 	return children;
 }
 
-// parses only code within main function. (FOR NOW..)
-//void parse() {
-//    mainNode.header = "0";
-//    mainNode.content = "MAIN NODE";
-//    string parsedToken = "";
-//    for (unsigned int i = 0; i < megaToken.size(); i++) {
-//        if (parsedToken.size() == 0 && megaToken[i] == ' ')
-//            continue;
-//        parsedToken += megaToken[i];
-//        if (megaToken[i] == ';') {
-//            i++;
-//            node newNode;
-//            newNode.header = mainNode.header + "." + toString(mainNode.children.size() + 1);
-//            newNode.add(parsedToken);
-//            parsedToken = "";
-//            mainNode.children.push_back(newNode);
-//        }
-//    }
+//
+//pair <vector <node>, int> parse(int index) {
+//	cout << "PARSING: " << index << '\n';
+//	vector <node> children;
+//	node child;
+//	for (unsigned int i = index; i < megaToken.size(); i++) {
+//		child.add(megaToken[i]);
+//		cout << "index = " << i << " " << "child got " << megaToken[i] << " added" << '\n';
+//		if (isMultiKeyword(i) == true) {
+//			cout << "I have found one here\n";
+//			for (unsigned int j = i + 1; j < megaToken.size(); j++) {
+//				child.add(megaToken[j]);
+//				cout << "index = " << j << " child got " << megaToken[j] << " added\n";
+//				if (megaToken[j + 1] == '{') {
+//					i = j + 3;
+//					child.children = parse(i).first;
+//					i = parse(i).second;
+//				}
+//			}
+//		}
+//
+//		if (megaToken[i + 1] == '}') {
+//			cout << "reached end of function / scope\n";
+//			return make_pair(children, i + 3);
+//		}
+//
+//		// special cases for header .h files
+//		if ((megaToken[i] == '>' && megaToken[i - 1] == 'h') || megaToken[i] == ';') {
+//			cout << "reached a statment / header file\n";
+//			i++;
+//			children.push_back(child);
+//			child.destroy();
+//		}
+//		index = i;
+//	}
+//	return make_pair(children, index);
 //}
 
 void traverse(node token) {
-    printf("NODE CONTENT:\n%s\n", token.header.c_str());
-    //printf("NODE PARENT ID: %d\n", token.parent->header.c_str()); off for now
+    printf("NODE CONTENT: %s\n", token.header.c_str());
     printf("# OF CHILDREN: %d\n\n", (int)token.children.size());
     for (unsigned int i = 0; i < token.children.size(); i++) {
         traverse(token.children[i]);
